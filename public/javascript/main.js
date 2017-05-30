7a2bd1f0890d10760fb4a995d08c618cfa65311d
Jan Pilik <snyshuem@gmail.com>
2017-05-30 22:01:25 +0200

    Re-make the menu on mobile and desktop. 1. Add animation, slide + fade in effect 2. Refine poor styling areas (colors, overlapping borders, spacing, font-size, padding for better user comfort) 3. Re-organize for modularity/BEM, separate header from navigation, clear-up naming; adjust css and js files to make the menu work 4. Fix the hamburger - shift to pixels from rem to adjust the effects with precision 5. Fix RWD - replace pixels with rem on header and nav blocks

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,22 +1,33 @@
+// ===================== Re-usable components =====================
+
+
 
 // ======= Toggle Visibility =======
 
 
-function toggleVisibility(className) {
-    var element = document.getElementsByClassName(className);
-   element[0].classList.toggle("navigation_display");
+function toggleVisibility(catchElement, className) {
+   var element = document.getElementsByClassName(catchElement);
+   element[0].classList.toggle(className);
 }
 
-function classSwitch(className) {
-	var element = document.getElementsByClassName(className);
-	element[0].classList.toggle("navigation__mobile-open");
+// ======= Toggle a Class =======
+
+function toggleClass(catchElement, className) {
+	var element = document.getElementsByClassName(catchElement);
+	element[0].classList.toggle(className);
 }
+
+
+
+// ===================== Implementions =====================
+
+
 
 // ======= Navigation - Toggle Visibility =======
 
 var navigation = document.getElementsByClassName("navigation__hamburger");
 
 navigation[0].addEventListener("click", function() {
-	toggleVisibility("navigation__ul");
-	classSwitch("navigation__hamburger")
+	toggleVisibility("navigation__ul", "navigation__ul--show");
+	toggleClass("navigation__hamburger", "navigation__mobile-open")
 });
